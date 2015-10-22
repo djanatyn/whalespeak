fde5575bdc14bd4b3507a7b5d525d1bf5d8729f1
Jonathan Strickland <djanatyn@gmail.com>
2015-10-22 10:10:25 -0700

    add animations and a jumbotron

--- web/public/js/whalespeak.js
+++ web/public/js/whalespeak.js
@@ -1,43 +1,48 @@
 $( document ).ready(function() {
   $( "#to-whalespeak" ).click(function(event) {
     var text_to_translate = $( "#text-to-translate" ).val();
-
-    $.ajax({
-      url: "api/translate/to",
-      data: '{ "text": "' + text_to_translate + '"}',
-      type: "POST",
-      dataType : "json",
-      success: function( json ) {
-          $( "#translated-text" ).text( json.translation );
-      },
-      error: function( xhr, status, errorThrown ) {
-          alert( "Sorry, there was a problem!" );
-          console.log( "Error: " + errorThrown );
-          console.log( "Status: " + status );
-          console.dir( xhr );
-      },
+    $( "#translated-text" ).slideUp(function() {
+      $.ajax({
+        url: "api/translate/to",
+        data: '{ "text": "' + text_to_translate + '"}',
+        type: "POST",
+        dataType : "json",
+        success: function( json ) {
+            $( "#translated-text" ).text( json.translation );
+            $( "#translated-text" ).slideDown();
+        },
+        error: function( xhr, status, errorThrown ) {
+            alert( "Sorry, there was a problem!" );
+            console.log( "Error: " + errorThrown );
+            console.log( "Status: " + status );
+            console.dir( xhr );
+        },
+      });
     });
   });
   $( "#from-whalespeak" ).click(function(event) {
     var text_to_translate = $( "#text-to-translate" ).val();
-
-    $.ajax({
-      url: "api/translate/from",
-      data: '{ "text": "' + text_to_translate + '"}',
-      type: "POST",
-      dataType : "json",
-      success: function( json ) {
-          var response = json.translation;
-          if (! response) { response = json.error };
-          
-          $( "#translated-text" ).text( response );
-      },
-      error: function( xhr, status, errorThrown ) {
-          alert( "Sorry, there was a problem!" );
-          console.log( "Error: " + errorThrown );
-          console.log( "Status: " + status );
-          console.dir( xhr );
-      },
+    
+    $( "#translated-text" ).slideUp(function() {
+      $.ajax({
+        url: "api/translate/from",
+        data: '{ "text": "' + text_to_translate + '"}',
+        type: "POST",
+        dataType : "json",
+        success: function( json ) {
+            var response = json.translation;
+            if (! response) { response = json.error };
+            
+            $( "#translated-text" ).text( response );
+            $( "#translated-text" ).slideDown();
+        },
+        error: function( xhr, status, errorThrown ) {
+            alert( "Sorry, there was a problem!" );
+            console.log( "Error: " + errorThrown );
+            console.log( "Status: " + status );
+            console.dir( xhr );
+        },
+      });
     });
   });
 });
